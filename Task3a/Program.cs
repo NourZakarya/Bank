7368ec9206a0472ba5b83ec47e5d7cb2738bb1bc
NourZakarya <142758979+NourZakarya@users.noreply.github.com>
2023-12-20 06:37:25 +0200

    Add files via upload

---
+++ Task3a/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static bool Vowels(string s)
+        {
+            foreach (char c in s)
+            {
+                if (IsVowel(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool IsVowel(char c)
+        {
+            char vow = char.ToLower(c);
+            return vow == 'a' || vow == 'e'|| vow == 'i'|| vow == 'o' || vow == 'u';
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a string:");
+                string s = Console.ReadLine();
+                if (!Vowels(s))
+                {
+                    throw new Exception("The string must contain vowels.");
+                }
+
+                Console.WriteLine("String contains vowels.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
