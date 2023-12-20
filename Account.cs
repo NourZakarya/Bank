6702d2e9c1b990f281c8ddfe0dc623687465fe10
Nour Zakarya <noorzakria25@gmail.com>
2023-12-20 06:22:02 +0200

    first commit

---
+++ Account.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+        public class Account
+        {
+            private decimal _balance;
+            private string _name;
+
+           // constructor to set value when create object
+            public Account(string name="none", decimal startingBalance=default)
+            {
+                _name = name;
+                _balance = startingBalance;
+            }
+
+
+        //public void Deposit(decimal amountToAdd)
+        //{
+
+        //    _balance += amountToAdd;
+        //}
+        public bool Deposit(decimal amountToAdd)
+        {
+            if (amountToAdd > 0)
+            {
+                _balance += amountToAdd;
+                return true;
+            }
+            return false;
+        }
+
+        //public void withdraw(decimal amount)
+        //    {
+
+        //       if (_balance >= amount)
+        //         _balance -= amount;
+        //      else
+        //        Console.WriteLine("Unsuccessful operation");
+        //}
+        public bool withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= _balance)
+            {
+
+                _balance -= amount;
+                return true;
+            }
+            return false;
+            
+        }
+        // make Name Read Only Property
+        public string Name
+            {
+                get { return _name; }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Name : {_name} , Balance = {_balance} ");
+            }
+
+
+        }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿namespace Bank
+{
+    enum MenuOption
+    {
+        Withdraw, Deposit, Print, Quit
+    }
+    public class Program
+    {
+        static MenuOption ReadUserOption()
+        {
+            MenuOption selection;
+            int s;
+            do
+            {
+                Console.WriteLine("Enter 0 to Withdraw :");
+                Console.WriteLine("Enter 1 to Deposit :");
+                Console.WriteLine("Enter 2 to Print :");
+                Console.WriteLine("Enter 3 to Quit :");
+                Console.Write("Enter Your Selection between 0 and 3 : ");
+                s = Convert.ToInt32(Console.ReadLine());
+                selection = (MenuOption)s;
+                if (selection < MenuOption.Withdraw || selection > MenuOption.Quit)
+                {
+                    Console.WriteLine("Envalid choice");
+                }
+            } while (selection < MenuOption.Withdraw || selection > MenuOption.Quit);
+            return selection;
+        }
+        static void Main(string[] args)
+        {
+            //Account account = new Account("Jakes Account", 200000);
+            //account.Print();
+            //account.Deposit(100);
+            //account.Print();
+
+            //account.withdraw(100000);
+            //account.Print();
+
+            //Console.WriteLine();
+            //Account a2 = new Account("NourEldin", 1000);
+            //a2.Print();
+            //a2.Deposit(2000);
+            //a2.Print();
+            //a2.withdraw(10000);
+            //a2.Print();
+
+            Account userAccount = new Account("nour", 1000);
+
+            while (true)
+            {
+                MenuOption Choice = ReadUserOption();
+
+
+                switch (Choice)
+                {
+                    case MenuOption.Deposit:
+                        DoDeposit(userAccount);
+                        break;
+                    case MenuOption.Withdraw:
+                        DoWithdraw(userAccount);
+                        break;
+                    case MenuOption.Print:
+                        DoPrint(userAccount);
+                        break;
+                    case MenuOption.Quit:
+                        Console.WriteLine("Exiting program. Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option selected. Please try again.");
+                        break;
+
+
+                }
+
+
+
+
+                static void DoDeposit(Account account)
+                {
+                    Console.Write("Enter deposit amount: ");
+                    decimal amount = Convert.ToDecimal(Console.ReadLine());
+
+                    if (account.Deposit(amount))
+                    {
+
+                        Console.WriteLine($"Deposit of {amount} successful.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid deposit amount. Please enter a valid number.");
+                    }
+                }
+
+                static void DoWithdraw(Account account)
+                {
+                    Console.Write("Enter withdrawal amount: ");
+                    decimal amount = Convert.ToDecimal(Console.ReadLine());
+                    if (account.withdraw(amount))
+                    {
+                        Console.WriteLine($"withdraw of {amount} successful.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid withdrawal amount. Please enter a valid number.");
+                    }
+                }
+
+                static void DoPrint(Account account)
+                {
+                    account.Print();
+                }
+            }
+        }
+    }
+}
